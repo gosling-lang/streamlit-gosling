f11c41c5da486cf1acb70b2b0bc07920d87e94ae
wangqianwen0418 <wangqianwen0418@163.com>
2022-07-08 15:34:00 -0400

    update example

--- streamlit_gosling/__init__.py
+++ streamlit_gosling/__init__.py
@@ -55,7 +55,7 @@ if not _RELEASE:
     # data = gos.matrix('/path/to/dataset.cool') # local dataset
 
     @st.cache
-    def point_chart(chr='1'):
+    def point_chart(chr='1', chartType='point chart'):
         data = gos.multivec(
             url="https://resgen.io/api/v1/tileset_info/?d=UvVPeLHuRDiYA3qwFlm7xQ",
             row="sample",
@@ -78,22 +78,33 @@ if not _RELEASE:
                 gos.Tooltip(field='sample', type='nominal')
             ],
         ).properties(
-            layout="linear", width=650, height=200, id='track-1',  experimental={"mouseEvents": True}
+            layout="linear", width=650, height=200, id='track-1',  experimental={"mouseEvents": True},
+            title=''
         )
+
+        if chartType=='bar chart':
+            track = track.mark_bar()
+        elif chartType == 'area chart':
+            track =track.mark_area().encode(row ='sample:N')
 
         chart = track.view(title='click to select an item')
 
         return chart
 
+    chartType = st.selectbox(
+     'Select a chart', ['point plot', 'bar chart', 'area chart']
+     )
+
     chr = st.selectbox(
      'Select a chromosome', [str(i) for i in range(1, 20)]
      )
+
     
-    eventType='rangeSelect'
     
-    result = streamlit_gosling(spec=point_chart(chr), id='id', height=350, eventType= eventType)
+    eventType='click'
+    
+    result = streamlit_gosling(spec=point_chart(chr, chartType), id='id', height=350, eventType= eventType)
 
     
     if result:
-        st.write('Below is a streamlit component')
-        st.write(eventType, result)
+        st.write(f'you {eventType}:', result)
