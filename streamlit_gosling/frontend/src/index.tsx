18d633b3f72531c79f8a8f594aa479053f74d1f2
etowahadams <etowahadams@gmail.com>
2024-06-05 17:59:29 -0400

    fix: remove strictmode

--- streamlit_gosling/frontend/src/index.tsx
+++ streamlit_gosling/frontend/src/index.tsx
@@ -2,9 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 import StreamlitGosling from "./steamlit-gosling"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <StreamlitGosling />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+ReactDOM.render(<StreamlitGosling />, document.getElementById("root"))
